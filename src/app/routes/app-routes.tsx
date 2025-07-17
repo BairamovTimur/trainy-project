bae1bb043938fc6504b41209f06f238a8f39da9a
Anvar <mts6060209@gmail.com>
2025-07-17 16:17:09 +0300

    task1_anvar[создал mainpage и 4 заглушки]

--- src/app/routes/app-routes.tsx
+++ src/app/routes/app-routes.tsx
@@ -1,10 +1,18 @@
 import { Navigate, Route, Routes } from 'react-router';
 import { MainPage } from '@/pages';
 import { ROOT_ROUTES, ROUTES } from '@/shared/routes';
+import { Counter } from '@/pages/Counter/Counter';
+import { AdvancedCounter } from '@/pages/AdvancedCounter/AdvancedCounter';
+import { UserList } from '@/pages/UserList/UserList';
+import { Table } from '@/pages/Table/Table';
 
 export const AppRoutes = () => (
 	<Routes>
 		<Route element={<MainPage />} path={ROUTES.MAIN.MAIN} />
+		<Route element={<Counter />} path={ROUTES.COUNTER.MAIN} />
+		<Route element={<AdvancedCounter />} path={ROUTES.ADVANCED_COUNTER.MAIN} />
+		<Route element={<UserList />} path={ROUTES.USER_LIST.MAIN} />
+		<Route element={<Table />} path={ROUTES.TABLE.MAIN} />
 		<Route element={<Navigate replace to={ROOT_ROUTES.MAIN} />} path='*' />
 	</Routes>
 );
---
+++ src/pages/Table/Table.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+
+export const Table = () => {
+    return(
+        <div className={styles.container}>
+            <h1>Table</h1>
+        </div>
+    )
+}
