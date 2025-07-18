a264a5c0c99d043c67744eeb11f0a1fe3199bd8c
Anvar <mts6060209@gmail.com>
2025-07-18 15:59:39 +0300

    fix: исправления после ревью

--- src/shared/components/NavigationLink/NavigationLink.tsx
+++ src/shared/components/NavigationLink/NavigationLink.tsx
@@ -1,23 +1,22 @@
-import { Link } from "react-router"
-import styles from "./styles.module.css"
+import { Link } from 'react-router';
+
+import styles from './styles.module.css';
 
 interface NavigationLinkProps {
-    title: string
-    to: string
-    icon: React.ReactNode
+	icon: React.ReactNode;
+	title: string;
+	to: string;
 }
 
-export const NavigationLink = ({title, to, icon}: NavigationLinkProps) => {
-    return(
-        <Link to={to} className={styles.navigationLink}>
-            <div className={styles.content}>
-                <h1>
-                    {title}
-                </h1>
-                <div className={styles.icon}>
-                    {icon} {to}
-                </div>
-            </div>
-        </Link> 
-    )
-}
+export const NavigationLink = ({ icon, title, to }: NavigationLinkProps) => {
+	return (
+		<Link className={styles.navigationLink} to={to}>
+			<div className={styles.content}>
+				<h1>{title}</h1>
+				<div className={styles.icon}>
+					{icon} {to}
+				</div>
+			</div>
+		</Link>
+	);
+};
